c4aa03e6a39969ccc68c04c87684f7f040ed3ab6
Netsuhe <Netsuhebekele@gmail.com>
2017-12-12 15:14:00 -0500

    minor change to about me

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var offset = $(window).scrollTop() - actualBlock.offset().top,
-	windowHeight = $(window).height();
-
-if( offset >= -windowHeight && offset <= 0 ) {
-	translateY = (-offset)*100/windowHeight;
-    scale = 1;
-	opacity = 1;
-} else if( offset > 0 && offset <= windowHeight ) {
-    scale = (1 - ( offset * 0.3/windowHeight));
-	opacity = ( 1 - ( offset/windowHeight) );
-	translateY = 0;
-	boxShadowBlur = 40*(offset/windowHeight);
-}
+// var offset = $(window).scrollTop() - actualBlock.offset().top,
+// 	windowHeight = $(window).height();
+//
+// if( offset >= -windowHeight && offset <= 0 ) {
+// 	translateY = (-offset)*100/windowHeight;
+//     scale = 1;
+// 	opacity = 1;
+// } else if( offset > 0 && offset <= windowHeight ) {
+//     scale = (1 - ( offset * 0.3/windowHeight));
+// 	opacity = ( 1 - ( offset/windowHeight) );
+// 	translateY = 0;
+// 	boxShadowBlur = 40*(offset/windowHeight);
+// }
